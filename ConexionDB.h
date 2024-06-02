5a7b4ef7b69957df0dfc15aa48965a89c6dc40b3
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-06-01 23:28:55 -0600

    Conexion

--- ConexionDB.h
+++ ConexionDB.h
@@ -10,7 +10,7 @@ private: MYSQL* conectar;
 public:
 	void abrir_conexion() {
 		conectar = mysql_init(0);
-		conectar = mysql_real_connect(conectar, "localhost", "root", "S3cur325@A", "ventas", 3306, NULL, 0);
+		conectar = mysql_real_connect(conectar, "", "root", "S3cur325@A", "ventas", 3306, NULL, 0);
 	}
 	void cerrar_conexion() {
 		mysql_close(conectar);
